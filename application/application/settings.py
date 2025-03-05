65f1e192fd84cb5a474f09bccbddcb1ea33df9cc
Devbhagadia <devbhagadia@gmail.com>
2025-03-05 16:21:56 +0530

    again edited index.html

--- application/application/settings.py
+++ application/application/settings.py
@@ -14,8 +14,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -56,7 +55,7 @@ ROOT_URLCONF = 'application.urls'
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "application" / "templates"],  # Ensure this path is correct
+        "DIRS": [BASE_DIR / "application" / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [

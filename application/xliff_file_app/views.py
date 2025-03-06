e272c84b4c82bbf94751ef3a47256c058f0c3798
Devbhagadia <devbhagadia@gmail.com>
2025-03-06 15:49:43 +0530

    changes

--- application/xliff_file_app/views.py
+++ application/xliff_file_app/views.py
@@ -185,7 +185,7 @@ def save_edits(request):
     if request.method == "POST":
         import json
         data = json.loads(request.body.decode('utf-8'))
-        translated_texts = data.get("translated_texts", [])
+        translated_texts = data.get("translations", [])
         print(f"DEBUG: Received {len(translated_texts)} translations")
 
         tmp_dir = "/tmp/"
